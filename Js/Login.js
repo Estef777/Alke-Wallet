ea2723e5b76eda43cfda8a97fc44a43e27020e71
Estef777 <munozestefaniasoto@gmail.com>
2024-03-28 19:08:53 -0300

    cambio login

--- Js/Login.js
+++ Js/Login.js
@@ -1,32 +1,33 @@
-//Primer usuario valido
-const userValido1 = "admin@alke.com";
+// Primer usuario válido
+const userValido1 = "hola@alkewallet.com";
 const passValida1 = "12345";
 
-//Segundo usuario valido
-const userValido2 = "admin2@alke.com";
+// Segundo usuario válido
+const userValido2 = "hola2@alkewallet.com";
 const passValida2 = "54321";
 
-//Rescatando datos del form para inicio de sesion
+// Rescatando datos del formulario para inicio de sesión
 const formulario = document.getElementById("formLogin");
 const email = document.getElementById("userEmail");
 const password = document.getElementById("userPassword");
 
-function login(e){
+// Función que inicia el inicio de sesión
+function login(e) {
     e.preventDefault();
-    console.log(email.value);
-    console.log(password.value);
-    
-    if(email.value == userValido1 && password.value == passValida1){
+
+    if (email.value === userValido1 && password.value === passValida1) {
+        // Si el primer usuario es correcto, dirigirlo al menú principal y almacenar el inicio de sesión en sessionStorage
         location.href = '../html/menu.html';
         sessionStorage.setItem("inicioDeSesion", 1);
-    } else{
-        if(email.value == userValido2 && password.value == passValida2){
-            location.href = '../html/menu.html';
-            sessionStorage.setItem("inicioDeSesion", 2);
-        }else{
-            alert("Email y/o contraseña erroneos");
-        }
+    } else if (email.value === userValido2 && password.value === passValida2) {
+        // Si el segundo usuario es correcto, dirigirlo al menú principal y almacenar el inicio de sesión en sessionStorage
+        location.href = '../html/menu.html';
+        sessionStorage.setItem("inicioDeSesion", 2);
+    } else {
+        // Si ninguno de los usuarios es correcto, mostrar un mensaje de error
+        alert("Correo electrónico y/o contraseña incorrectos");
     }
 }
 
-formulario.addEventListener("submit", login)
+// Ejecutar la función login al enviar el formulario
+formulario.addEventListener("submit", login);
