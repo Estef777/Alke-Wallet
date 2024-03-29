d49b0919ab877362ff971943f884bfdc755c3486
Estef777 <munozestefaniasoto@gmail.com>
2024-03-29 11:13:02 -0300

    login actualizacion

--- assets/Js/Login.js
+++ assets/Js/Login.js
@@ -1,28 +1,70 @@
+// Primer usuario válido
 const userValido1 = "hola@alkewallet.com";
-        const passValida1 = "12345";
+const passValida1 = "12345";
 
-        const userValido2 = "hola2@alkewallet.com";
-        const passValida2 = "54321";
+// Segundo usuario válido
+const userValido2 = "hola2@alkewallet.com";
+const passValida2 = "54321";
 
-        const formulario = document.getElementById("formLogin");
-        const email = document.getElementById("userEmail");
-        const password = document.getElementById("userPassword");
-        const menuLink = document.getElementById("menuLink");
+// Rescatando datos del formulario para inicio de sesión
+const formulario = document.getElementById("formLogin");
+const email = document.getElementById("userEmail");
+const password = document.getElementById("userPassword");
 
-        function login(e) {
-            e.preventDefault();
+// Función que inicia el inicio de sesión
+function login(e) {
+    e.preventDefault();
 
-            if (email.value === userValido1 && password.value === passValida1) {
-                // Si el primer usuario es correcto, dirigirlo al menú principal
-                menuLink.click(href="../../menu.html"); // Simular clic en el enlace
-            } else if (email.value === userValido2 && password.value === passValida2) {
-                // Si el segundo usuario es correcto, dirigirlo al menú principal
-                menuLink.click(href="../../menu.html"); // Simular clic en el enlace
-            } else {
-                // Si ninguno de los usuarios es correcto, mostrar un mensaje de error
-                alert("Correo electrónico y/o contraseña incorrectos");
-            }
-        }
+    if (email.value === userValido1 && password.value === passValida1) {
+        // Si el primer usuario es correcto, dirigirlo al menú principal y almacenar el inicio de sesión en sessionStorage
+        location.href = 'menu.html';
+        sessionStorage.setItem("inicioDeSesion", 1);
+    } else if (email.value === userValido2 && password.value === passValida2) {
+        // Si el segundo usuario es correcto, dirigirlo al menú principal y almacenar el inicio de sesión en sessionStorage
+        location.href = 'menu.html';
+        sessionStorage.setItem("inicioDeSesion", 2);
+    } else {
+        // Si ninguno de los usuarios es correcto, mostrar un mensaje de error
+        alert("Correo electrónico y/o contraseña incorrectos");
+    }
+}
+
+// Ejecutar la función login al enviar el formulario
+formulario.addEventListener("submit", login);
+
+
+
+
+
+
+
+
+
+// const userValido1 = "hola@alkewallet.com";
+//         const passValida1 = "12345";
+
+//         const userValido2 = "hola2@alkewallet.com";
+//         const passValida2 = "54321";
+
+//         const formulario = document.getElementById("formLogin");
+//         const email = document.getElementById("userEmail");
+//         const password = document.getElementById("userPassword");
+//         const menuLink = document.getElementById("menuLink");
+
+//         function login(e) {
+//             e.preventDefault();
+
+//             if (email.value === userValido1 && password.value === passValida1) {
+//                 // Si el primer usuario es correcto, dirigirlo al menú principal
+//                 menuLink.click(href="../../menu.html"); // Simular clic en el enlace
+//             } else if (email.value === userValido2 && password.value === passValida2) {
+//                 // Si el segundo usuario es correcto, dirigirlo al menú principal
+//                 menuLink.click(href="../../menu.html"); // Simular clic en el enlace
+//             } else {
+//                 // Si ninguno de los usuarios es correcto, mostrar un mensaje de error
+//                 alert("Correo electrónico y/o contraseña incorrectos");
+//             }
+//         }
 
 
 
